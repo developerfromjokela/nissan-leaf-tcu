53dc83d221574a0bc2d9367a1ce23a2b61364cf3
Developer From Jokela <developerfromjokela@gmail.com>
2025-04-24 16:24:31 +0300

    Add new SOC

--- scripts/evinfoze0parser.py
+++ scripts/evinfoze0parser.py
@@ -1,8 +1,10 @@
 from os import listdir
 from os.path import isfile, join
 
-def parse_evinfo(byte_data):
-    evinfo_ver = int(byte_data[0])
+def parse_evinfo(byte_data, aze0=False):
+    rangeinfo_len = int(byte_data[0])
+
+    evinfo_len = int(byte_data[8])
 
     # Charge & Pre-AC state
     charge_state = (byte_data[9] >> 6) & 0b11
@@ -13,11 +15,11 @@ def parse_evinfo(byte_data):
     quick_charging = bool((byte_data[9] & 0b10000000) >> 7)
 
     chg_time_1 = (
-        (byte_data[10] << 2) | ((byte_data[11] & 0b11000000) >> 6)
+        (byte_data[10] << 3) | ((byte_data[11] & 0b11100000) >> 5)
     )
 
     chg_time_2 = (
-        ((byte_data[11] & 0b00111111) << 4) | ((byte_data[12] & 0b11110000) >> 4)
+        ((byte_data[11] & 0b00011111) << 6) | ((byte_data[12] & 0b11111100) >> 2)
     )
 
     # Drive info
@@ -28,6 +30,7 @@ def parse_evinfo(byte_data):
 
 
     # Battery info
+    soc_display = 0
     chargebars = (
         ((byte_data[18] & 0b00000011) << 2) | ((byte_data[19] & 0b11000000) >> 6)
     )
@@ -41,23 +44,30 @@ def parse_evinfo(byte_data):
     soh = (
         ((byte_data[15] & 0b00111111) << 1) | ((byte_data[16] & 0b10000000) >> 7)
     )
-    #param16_17 = (
-    #    ((byte_data[16] & 0b01111111) << 4) | ((byte_data[17]) >> 4)
-    #)
+    param21 = 0
+
     byte1 = byte_data[16]  # 01010001 in binary
     byte2 = byte_data[17]  # 00100000 in binary
 
-    # Extract bits:
-    # - Byte 1: Ignore first bit (0), take last 7 bits (1010001)
-    bits_from_byte1 = byte1 & 0x7F  # Mask to get last 7 bits: 1010001 (81 decimal)
-    # Shift left by 4 to make room for byte2's bits
-    shifted_bits = bits_from_byte1 << 4
+    if aze0:
+        soc_display = (
+                ((byte1 & 0b01111111) << 3) | ((byte2 & 0b11100000) >> 5)
+        )
+        soc_display = soc_display/10
+        param16_17 = (
+            (byte_data[20] << 4) | ((byte_data[21] & 0b11110000) >> 4)
+        )
+        param21 = (byte_data[21] & 0b00001111)
+    else:
+        bits_from_byte1 = byte1 & 0x7F  # Mask to get last 7 bits: 1010001 (81 decimal)
+        # Shift left by 4 to make room for byte2's bits
+        shifted_bits = bits_from_byte1 << 4
 
-    # - Byte 2: Take first 4 bits (0010)
-    bits_from_byte2 = (byte2 >> 4) & 0x0F  # Shift right 4, mask to get 0010 (2 decimal)
+        # - Byte 2: Take first 4 bits (0010)
+        bits_from_byte2 = (byte2 >> 4) & 0x0F  # Shift right 4, mask to get 0010 (2 decimal)
 
-    # Combine the bits
-    param16_17 = shifted_bits | bits_from_byte2  # 10100010010 in binary
+        # Combine the bits
+        param16_17 = shifted_bits | bits_from_byte2  # 10100010010 in binary
 
 
     range_acon = byte_data[2]
@@ -67,7 +77,8 @@ def parse_evinfo(byte_data):
     alertstate = byte_data[6]
 
     return {
-        "ver": evinfo_ver,
+        "rangeinfo_len": rangeinfo_len,
+        "evinfo_len": evinfo_len,
         "acon": range_acon,
         "acoff": range_acoff,
         "pluggedin":pluggedin,
@@ -82,28 +93,30 @@ def parse_evinfo(byte_data):
         "parked": park_gear,
         "direction_forward": drive_forward,
         "soc": soc,
+        "soc_display": soc_display,
         "gids": gids,
         "soh": soh,
         "gids_relative": param16_17,
+        "param21": param21,
         "capacity_bars": capacity_bars,
         "full_chg": chg_time_1,
         "limit_chg": chg_time_2,
     }
 
 if __name__ == "__main__":
-    onlyfiles = [f for f in listdir("../captured_data/ze0/runtimehistory") if isfile(join(
-        "../captured_data/ze0/runtimehistory", f)) and f.endswith(".bin")]
+    onlyfiles = [f for f in listdir("../captured_data/aze0/qc_charging/capt") if isfile(join(
+        "../captured_data/aze0/qc_charging/capt", f)) and f.endswith(".bin")]
     onlyfiles.sort()
     dtrows = []
     for file in onlyfiles:
-        with open(f"captured_data/ze0/runtimehistory/{file}", "rb") as f:
+        with open(f"../captured_data/aze0/qc_charging/capt/{file}", "rb") as f:
             data = f.read()
             ev_data = data[153:]
 
             try:
-                result = parse_evinfo(ev_data)
+                result = parse_evinfo(ev_data, aze0=True)
                 for key, value in result.items():
-                    print(f"{key}: {value} (0x{value:02X})")
+                    print(f"{key}: {value} {type(value)}")
                 print("----------------", file)
             except ValueError as e:
                 print(f"Error: {e}")
